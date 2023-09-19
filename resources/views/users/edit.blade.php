d78a2b483e5b83e2b772e4391eb9cc6a0648d635
unknown <joao.ugui@gmail.com>
2023-09-18 23:48:13 -0300

    little corrections

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -79,33 +79,19 @@
     </div>
 
 
-    <form method="POST" action="{{ route('delete', ['id' => $user->id]) }}" id="delete-form">
+    <form method="POST" action="{{ route('delete', ['id' => $user->id]) }}" id="delete-form" onsubmit="ConfirmDelete()">
         @csrf
-        <div class="col-md-6">
-            <div id="confirm" style="display: none;">
-                <div class="form-group">
-                    <label for="confirm"><strong>This action is definitive, are you sure?</strong></label>
-                </div>
-                <button type="submit" style="display: none;" id="confirm-delete" class="btn btn-danger">Confirm Delete</button>
-            </div>
-        </div>
-        <button type="button" id="show-confirm" class="btn btn-danger">Delete User</button>
+        <button type="submit" id="confirm" class="btn btn-danger">Delete User</button>
     </form>
     
 
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
     <script>
-        $(document).ready(function() {
-            // Quando o botão "Delete User" for clicado
-            $("#show-confirm").click(function() {
-                // Oculta o botão "Delete User"
-                $(this).hide();
-                // Exibe o campo de confirmação
-                $("#confirm").show();
-                // Exibe o botão "Confirm Delete"
-                $("#confirm-delete").show();
-            });
-        });
+        
+        function ConfirmDelete() {
+            confirm("This action is definitive, are you sure?")
+        };
+
     </script>
 @endsection
